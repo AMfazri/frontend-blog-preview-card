faea40684cacac3de519bb3ba15ac624d8384924
AMfazri <fazri.yaafazri@gmail.com>
2026-01-07 22:01:07 +0700

    feat: add base semantic html5 structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Article from './article'
 
 function App() {
   
   return (
     <>
       <main className='container'>
-
+      <Article/>
       </main>
     </>
   )
---
+++ src/article.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import blogimage from './assets/images/blog-image.png'
+import imageavatar from './assets/images/image-avatar.webp'
+
+function Article() {
+    return(<>
+    <article className='card'>
+        <img src={blogimage} alt="article image" />
+        <h3 className='label'>Learning</h3>
+        <h2 className='pub-date'>Published 21 Dec 2023</h2>
+        <h1 className='heading'>HTML & CSS foundations</h1>
+        <p className='explains'>
+        These languages are the backbone of every website, defining structure, content, and presentation.</p>
+        <img src={imageavatar} alt="profil photo" />
+        <h4 className='name'>Greg Hooper</h4>
+      </article>
+    </>)
+}
+
+export default Article;
+
+
+
+  
+
+  
+
+
+  
+  
