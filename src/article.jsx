0850f3ded927eec0ee204763f25602c7b453d175
AMfazri <fazri.yaafazri@gmail.com>
2026-01-07 22:58:46 +0700

    feat: built main layout structure

--- src/article.jsx
+++ src/article.jsx
@@ -5,14 +5,17 @@ import imageavatar from './assets/images/image-avatar.webp'
 function Article() {
     return(<>
     <article className='card'>
-        <img src={blogimage} alt="article image" />
+        <img className='article-image' src={blogimage} alt="article image" />
         <h3 className='label'>Learning</h3>
         <h2 className='pub-date'>Published 21 Dec 2023</h2>
         <h1 className='heading'>HTML & CSS foundations</h1>
         <p className='explains'>
         These languages are the backbone of every website, defining structure, content, and presentation.</p>
-        <img src={imageavatar} alt="profil photo" />
-        <h4 className='name'>Greg Hooper</h4>
+        <div className='profil'>
+            <img className='profil-photo' src={imageavatar} alt="profil photo" />
+            <h4 className='name'>Greg Hooper</h4>
+        </div>
+        
       </article>
     </>)
 }
